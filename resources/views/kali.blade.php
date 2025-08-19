cde8b5787a484c478059ab890dde90bb33d628db
RizkyRatna17 <rizkyratna17@gmail.com>
2025-08-19 14:53:31 +0700

    First Commit

---
+++ resources/views/kali.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Belajar Laravel</title>
+</head>
+<body>
+<h1>Tambah Data</h1>
+{{-- //untuk mengirim data ke controller --}}
+<form action="{{route('store_kali')}}" method="post">
+    @csrf
+    <label for="">Angka 1</label>
+    <input type="number" name="angka1">
+    <br><br>
+     <label for="">Angka 2</label>
+     <input type="number" name="angka2">
+     <br><br>
+     <button type="submit">Proses</button>
+   <a href="{{url()->previous()}}">Kembali</a>
+</form>
+<h3>Hasilnya : {{$kali ?? 0}} </h3>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+// get(melihat/read) , post(menyimpan/create) , put(update) , delete(hapus/delete)
+
+Route::get('belajar', [\App\Http\Controllers\BelajarController::class, 'index']);
+Route::get('login', [\App\Http\Controllers\LoginController::class, 'login'])->name('login');
+Route::post('login_action', [\App\Http\Controllers\LoginController::class, 'loginAction'])->name('login_action');
+
+Route::resource('dashboard', App\Http\Controllers\DashboardController::class); //untuk mengakses dashboard dan halaman resource lainnya
+
+Route::get("call_name", [App\Http\Controllers\BelajarController::class, 'getCallName']);
+Route::get("tambah", [App\Http\Controllers\BelajarController::class, 'tambah'])->name('tambah'); //jika pake route ditambah nama
+Route::post('store_tambah', [App\Http\Controllers\BelajarController::class, 'storeTambah'])->name('store_tambah'); // untuk menyimpan data dari form tambah
+Route::get("kurang", [App\Http\Controllers\BelajarController::class, 'kurang'])->name('kurang');
+Route::post('store_kurang', [App\Http\Controllers\BelajarController::class, 'storeKurang'])->name('store_kurang');
+Route::get("bagi", [App\Http\Controllers\BelajarController::class, 'bagi'])->name('bagi');
+Route::post('store_bagi', [App\Http\Controllers\BelajarController::class, 'storeBagi'])->name('store_bagi');
+Route::get("kali", [App\Http\Controllers\BelajarController::class, 'kali'])->name('kali');
+Route::post('store_kali', [App\Http\Controllers\BelajarController::class, 'storeKali'])->name('store_kali');
